f42f21e45636f5bba779e4193dd2baf34e530588
mickeyminion14 <sarthakagrawal6@gmail.com>
2018-06-15 16:13:53 +0530

    first commti

---
+++ src/app/lol/lol.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { templateJitUrl } from '@angular/compiler';
+
+@Component({
+    selector: 'app-lol',
+  templateUrl: './lol.component.html'
+})
+
+export class LolComponent { }
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { templateJitUrl } from '@angular/compiler';
+
+@Component({
+    selector: 'app-server',
+  templateUrl: './server.component.html'
+})
+
+export class ServerComponent { }
